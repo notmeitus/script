1cbe2cd96a1ce28a83aedd1a5afac23ee05e0afd
shiva! <notlinux3@gmail.com>
2024-11-06 20:39:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let b = document.body;
+
+document.querySelector('.btn1').onclick = gbb;
+function gbb(){
+  b.style.backgroundColor = "blue";
+ }
+ 
+ document.querySelector('.btn2').onclick = bg;
+ function bg(){
+  b.style.backgroundColor = "green";
+ }
+ 
